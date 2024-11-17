1a9c716a4f5c2e48c5fb602a5d1d75ed194ed79c
masum184e <masum184e@gmail.com>
2024-11-17 22:16:43 +0600

    updated

--- odyssey.sql
+++ odyssey.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2024 at 06:52 AM
+-- Generation Time: Nov 17, 2024 at 05:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,58 @@ SET time_zone = "+00:00";
 --
 -- Database: `odyssey`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  `otp` int(6) DEFAULT NULL,
+  `otp_created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `booking_id` int(11) NOT NULL,
+  `driver_id` int(11) NOT NULL,
+  `renter_id` int(11) NOT NULL,
+  `pickup_datetime` datetime NOT NULL,
+  `dropoff_datetime` datetime NOT NULL,
+  `pickup_location` varchar(255) NOT NULL,
+  `dropoff_location` varchar(255) NOT NULL,
+  `number_of_passengers` int(11) NOT NULL,
+  `number_of_stoppages` int(11) NOT NULL,
+  `booking_status` enum('Pending','In Progress','Completed','Cancelled') NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaints`
+--
+
+CREATE TABLE `complaints` (
+  `complaint_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_type` enum('driver','renter') NOT NULL,
+  `message` text NOT NULL,
+  `document` varchar(255) DEFAULT NULL,
+  `status` enum('pending','completed') DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -47,6 +99,23 @@ CREATE TABLE `drivers` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `packages`
+--
+
+CREATE TABLE `packages` (
+  `vehicle_id` int(11) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `base_price` decimal(10,2) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `ac_status` tinyint(1) NOT NULL,
+  `per_km_cost` decimal(10,2) NOT NULL,
+  `per_hour_cost` decimal(10,2) NOT NULL,
+  `approximate_seats` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `renters`
 --
 
@@ -63,9 +132,100 @@ CREATE TABLE `renters` (
   `otp_created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suspensions`
+--
+
+CREATE TABLE `suspensions` (
+  `suspend_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_type` enum('driver','renter') NOT NULL,
+  `begin_at` datetime NOT NULL,
+  `end_at` datetime DEFAULT NULL,
+  `cause` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trips`
+--
+
+CREATE TABLE `trips` (
+  `trip_id` int(11) NOT NULL,
+  `booking_id` int(11) NOT NULL,
+  `trip_start_time` datetime NOT NULL,
+  `trip_end_time` datetime DEFAULT NULL,
+  `renter_countdown_status` enum('Pending','In Progress','Completed','Cancelled') NOT NULL DEFAULT 'Pending',
+  `driver_countdown_status` enum('Pending','In Progress','Completed','Cancelled') NOT NULL DEFAULT 'Pending',
+  `countdown_timer` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip_feedbacks`
+--
+
+CREATE TABLE `trip_feedbacks` (
+  `feedback_id` int(11) NOT NULL,
+  `rating` tinyint(1) NOT NULL CHECK (`rating` between 1 and 5),
+  `feedback` text DEFAULT NULL,
+  `trip_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `vehicle_id` int(11) NOT NULL,
+  `driver_id` int(11) NOT NULL,
+  `license_plate_number` varchar(20) NOT NULL,
+  `mileage` decimal(10,2) NOT NULL,
+  `number_of_seats` int(11) NOT NULL,
+  `chasis_number` varchar(50) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `year` year(4) NOT NULL,
+  `color` varchar(30) NOT NULL,
+  `owner_mobile_number` varchar(14) NOT NULL,
+  `owner_image` varchar(255) NOT NULL,
+  `main_image` varchar(255) NOT NULL,
+  `front_image` varchar(255) NOT NULL,
+  `back_image` varchar(255) NOT NULL,
+  `left_image` varchar(255) NOT NULL,
+  `interior_image` varchar(255) NOT NULL,
+  `right_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
+-- Indexes for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD PRIMARY KEY (`complaint_id`);
 
 --
 -- Indexes for table `drivers`
@@ -76,6 +236,12 @@ ALTER TABLE `drivers`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`vehicle_id`);
+
+--
 -- Indexes for table `renters`
 --
 ALTER TABLE `renters`
@@ -84,8 +250,50 @@ ALTER TABLE `renters`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `suspensions`
+--
+ALTER TABLE `suspensions`
+  ADD PRIMARY KEY (`suspend_id`);
+
+--
+-- Indexes for table `trips`
+--
+ALTER TABLE `trips`
+  ADD PRIMARY KEY (`trip_id`);
+
+--
+-- Indexes for table `trip_feedbacks`
+--
+ALTER TABLE `trip_feedbacks`
+  ADD PRIMARY KEY (`feedback_id`);
+
+--
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`vehicle_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaints`
+--
+ALTER TABLE `complaints`
+  MODIFY `complaint_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `drivers`
@@ -94,10 +302,40 @@ ALTER TABLE `drivers`
   MODIFY `driver_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `packages`
+--
+ALTER TABLE `packages`
+  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `renters`
 --
 ALTER TABLE `renters`
   MODIFY `renter_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `suspensions`
+--
+ALTER TABLE `suspensions`
+  MODIFY `suspend_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trips`
+--
+ALTER TABLE `trips`
+  MODIFY `trip_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trip_feedbacks`
+--
+ALTER TABLE `trip_feedbacks`
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
